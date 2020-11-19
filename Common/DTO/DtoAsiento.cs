e7472831b0e9755c96a7d6a3a3e88d80814d0347
Jessica Nuñez <jessica_rbd64@hotmail.com>
2020-11-18 22:37:37 -0300

    dtoasiento

--- Common/DTO/DtoAsiento.cs
+++ Common/DTO/DtoAsiento.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +11,23 @@ namespace Common.DTO
     public class DtoAsiento
     {
         public int numeroAsiento { get; set; }
+
+        [DisplayName("Tipo")]
         public string tipo { get; set; }
         public Nullable<int> cantidad { get; set; }
         public Nullable<double> precio { get; set; }
         public Nullable<bool> seleccionado { get; set; }
+
+        [DisplayName("Fila")]
+        [StringLength(15, ErrorMessage = "La {0} no debe superar los {1} carácteres")]
         public string fila { get; set; }
+
+        [DisplayName("Desde")]
+        [Required(ErrorMessage = "Se debe ingresar desde que número de asiento empieza!")]
         public int desde { get; set; }
+
+        [DisplayName("Hasta")]
+        [Required(ErrorMessage = "Se debe ingresar hasta que número de asiento finaliza!")]
         public int hasta { get; set; }
     }
 }
