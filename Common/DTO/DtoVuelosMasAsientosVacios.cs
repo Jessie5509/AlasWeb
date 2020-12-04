cf6bbc071bbed413f58e01d7bc2973c159f8d555
Jessica Nuñez <jessica_rbd64@hotmail.com>
2020-12-03 21:10:45 -0300

    Reportes

---
+++ Common/DTO/DtoVuelosMasAsientosVacios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DTO
+{
+    public class DtoVuelosMasAsientosVacios
+    {
+        public DateTime fechaInicio { get; set; }
+        public DateTime fechaFin { get; set; }
+
+    }
+}
