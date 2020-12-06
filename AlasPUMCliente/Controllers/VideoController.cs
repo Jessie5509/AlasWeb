40713625fd274603c5bd33c78f2179a603395509
MarcossBonet <MarcossBonet@users.noreply.github.com>
2020-12-06 16:57:26 -0300

    mas del coso

--- AlasPUMCliente/Controllers/VideoController.cs
+++ AlasPUMCliente/Controllers/VideoController.cs
@@ -18,13 +18,21 @@ namespace AlasPUMCliente.Controllers
             return View();
         }
 
-        public ActionResult VerVideo(DtoCliente dto)
+        public ActionResult VerVideo()
         {
-
+            DtoCliente dto = new DtoCliente();
+            dto.documento = "12345";
             IVideo videoproxy = new ProxyVideo(new ClaseVideo());
             string video = videoproxy.AgregarVideo(dto);
             ViewBag.UrlVideo = video;
+
+            if (!string.IsNullOrEmpty(video))
+            {
+                return Redirect(video);
+            }
+
             return View();
+
 
         }
     }
